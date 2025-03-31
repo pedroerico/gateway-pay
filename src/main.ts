d5789a94daef8f90e39c6722b95d9803bc84cdd7
Pedro Erico <pedroerico.desenvolvedor@gmail.com>
2025-03-31 20:48:18 -0300

    build: configurando App e main para novo layout

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,27 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
+import { createVuetify } from 'vuetify'
+import { createPinia } from 'pinia'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 import App from './App.vue'
+import router from './router'
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
 
-createApp(App).mount('#app')
+const pinia = createPinia()
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  theme: {
+    defaultTheme: 'light'
+  }
+})
+
+const app = createApp(App)
+
+app.use(pinia)
+app.use(vuetify)
+app.use(router)
+
+app.mount('#app')
